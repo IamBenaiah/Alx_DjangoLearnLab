678cdf1f67d5d74055aef788e29e0fede7bd4b41
iambenaiah <benaiah5627@gmail.com>
2025-03-13 22:57:21 +0100

    Added serializers, views, and API URLs

--- api/serializers.py
+++ api/serializers.py
@@ -4,4 +4,4 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = '__all__'  # This includes all fields from the Book model
--- api/views.py
+++ api/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 
 @api_view(['GET'])
 def book_list(request):
+class BookList(ListAPIView):
     books = Book.objects.all()
     serializer = BookSerializer(books, many=True)
     return Response(serializer.data)
