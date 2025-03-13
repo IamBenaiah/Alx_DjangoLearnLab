f00376ac3c5df5712991f48492d8008e19131017
iambenaiah <benaiah5627@gmail.com>
2025-03-13 21:22:45 +0100

    Initial commit - Setup Django REST Framework project

---
+++ api/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Book
+from .serializers import BookSerializer
+
+@api_view(['GET'])
+def book_list(request):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "API is working!"})
+
