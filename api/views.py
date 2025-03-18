c6ad956466d12646e69376795a110d94cc0ebe19
iambenaiah <benaiah5627@gmail.com>
2025-03-18 13:08:45 +0100

    Added books API endpoint using DRF

--- api/views.py
+++ api/views.py
@@ -1,4 +1,3 @@
-from rest_framework.response import Response
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
@@ -6,9 +5,3 @@ from .serializers import BookSerializer
 class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        return Response(serializer.data, status=201)  # Ensure this is inside a function
