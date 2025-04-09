005a24aaa2d043011c302e52cd87d407f6f40a72
iambenaiah <benaiah5627@gmail.com>
2025-04-09 14:37:17 +0100

    Commit

--- api/models.py
+++ api/models.py
@@ -1,8 +1,6 @@
-from django.db import models
+class Author(models.Model):
+    name = models.CharField(max_length=100)
 
 class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.title
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
